a132b5981c3bbb06ce2693bce1d2d0ff583802d7
sukhd <centaurgolden@gmail.com>
2020-05-21 16:33:13 -0500

    added room png

---
+++ imgs/css/css.js
@@ -0,0 +1,24 @@
+export default  [
+    {
+    name = "CSS Room",
+    img = "imgs/css/room.png",
+    code = "css/css-room/index.html"
+},
+    {
+    name = "Markdown Editor",
+    img = "imgs/css/markdown.png",
+    code = "css/markdown-editor/index.html"
+},
+    {
+    name = "Colorful Dots",
+    img = "imgs/css/colorful-dots.png",
+    code = "css/colorful-dots.html"
+},
+    {
+    name = "D3 Circles",
+    img = "imgs/css/d3-circles.png",
+    code = "css/d3-circles.html"
+},
+
+
+]
